288fb0a2cbb9bbe01a25287153bfbe3e7fab94b2
Harish Visweswaran <harish.visweswaran@us.ibm.com>
2019-04-28 14:45:29 -0400

    Add consumer for images from storage and reorg files

---
+++ consumer/consumer.py
@@ -0,0 +1,41 @@
+from PIL import Image
+from io import BytesIO
+import pickle
+import json
+import numpy as np
+from pykafka import KafkaClient
+from pykafka.common import OffsetType
+
+
+def gen_client(hosts="127.0.0.1:9092", topic_name='test'):
+    client = KafkaClient(hosts=hosts)
+    topic = client.topics[topic_name]
+    return client, topic
+
+
+def decode(msg):
+    msg = pickle.loads(msg)
+    img = Image.open(BytesIO(msg['img_bytes']))
+    msg['img_arr'] = np.array(img)
+    del(msg['img_bytes'])
+    return msg
+
+
+def model(msg):
+    """Call the model from here maybe"""
+    print('Request Time:', msg['req_time'],
+          'Image Dimensions:', msg['img_arr'].shape)
+
+
+if __name__ == "__main__":
+    client, topic = gen_client(hosts="127.0.0.1:9092", topic_name='test')
+    consumer = topic.get_simple_consumer()
+    for msg in consumer:
+        if msg is not None:
+            msg = decode(msg.value)
+            model(msg)
+
+
+# To dos:
+# Dockerize the application
+# Improve the consumer - currently it is a simple consumer
